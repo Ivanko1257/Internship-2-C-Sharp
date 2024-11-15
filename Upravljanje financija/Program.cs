d3a2de35eee9667c80499f941d9bfde6656e67b0
Ivanko1257 <ivan@vukusic.net>
2024-11-15 21:05:43 +0100

    Home page i unos operacije

--- Upravljanje financija/Program.cs
+++ Upravljanje financija/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.WriteLine("1 - Korisnici");
+Console.WriteLine("2 - Računi");
+Console.WriteLine("3 - Izlaz iz aplikacije");
+int operation_num;
+while (true)
+{
+    Console.Write("Upišite broj željene operacije");
+    int.TryParse(Console.ReadLine(), out operation_num);
+    if (operation_num == 1 || operation_num == 2 || operation_num == 3)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Nemoguća operacija.");
+    }
+}
