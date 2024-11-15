0e9569fe98188a09df403bf055b9d33e39d1c8fc
Ivanko1257 <ivan@vukusic.net>
2024-11-16 00:12:44 +0100

    Brisanje i uređivanje korisnika

--- Upravljanje financija/Program.cs
+++ Upravljanje financija/Program.cs
@@ -1,4 +1,6 @@
-﻿var Accounts = new Dictionary<int, Tuple<string, string, DateTime>>()
+﻿using System.Runtime.InteropServices;
+
+var Accounts = new Dictionary<int, Tuple<string, string, DateTime>>()
     {
         {0, Tuple.Create("Mujo", "Mujić", new DateTime(1996, 1, 31))},
         {1, Tuple.Create("Haso", "Hasić", new DateTime(1994, 4, 7)) }
@@ -54,16 +56,148 @@ while (true)
                 }
                 break;
             case 2:
-                Console.Clear ();
-                break;
+                {
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Upišite ID ako želite brisati korsinika preko ID-a ili IME I PREZIME ako želite brisati korisnika preko toga, a ako se želite vratiti na početni zaslon upišite HOME:  ");
+                        var delete_input = Console.ReadLine();
+                        if (delete_input=="HOME")
+                        {
+                            break;
+                        }
+                        if (delete_input == "ID")
+                        {
+                            Console.Write("Upišite ID korisnika");
+                            int.TryParse(Console.ReadLine(), out var delete_input_id);
+                            foreach (var keys in Accounts.Keys)
+                            {
+                                if (keys==delete_input_id)
+                                {
+                                    Console.WriteLine($"Jeste li sigurni da želite izbrisati {Accounts[delete_input_id]}? Upišite YES ako da, NO ako ne");
+                                    var delete_affirmation = Console.ReadLine();
+                                    if (delete_affirmation == "YES")
+                                    {
+                                        delete_input = "done";
+                                        Accounts.Remove(delete_input_id);
+                                        Console.WriteLine("Korisnik uspiješno izbrisan. Vračamo vas na početni zaslon");
+                                        Thread.Sleep(2000);
+                                        break;
+
+                                    }
+                                    else if (delete_affirmation == "NO")
+                                    {
+                                        Console.WriteLine("Zahtjev odbijen. Vrračamo vas na početni zaslon");
+                                        delete_input = "done";
+                                        break;
+                                    }
+                                }
+                            }
+                            if(delete_input!="done")
+                            {
+                                Console.WriteLine("Korisnik ne postoji te je brisanje bilo neuspiješno. Pokušajte ponovo.");
+                                Thread.Sleep(2000);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        if (delete_input=="IME I PREZIME")
+                        {
+                            Console.Write("Upišite Ime korisnika: ");
+                            var delete_name= Console.ReadLine();
+                            Console.Write("Upišite Prezime korisnika: ");
+                            var delete_surname=Console.ReadLine();
+                            foreach(var key in Accounts.Keys)
+                            {
+                                if (Accounts[key].Item1 == delete_name && Accounts[key].Item2 == delete_surname)
+                                {
+                                    Console.WriteLine($"Jeste li sigurni da želite izbrisati {Accounts[key]}? Upišite YES ako da, NO ako ne");
+                                    var delete_affirmation = Console.ReadLine();
+                                    if (delete_affirmation == "YES")
+                                    {
+                                        delete_input = "done";
+                                        Accounts.Remove(key);
+                                        Console.WriteLine("Korisnik uspiješno izbrisan. Vračamo vas na početni zaslon");
+                                        Thread.Sleep(2000);
+                                        break;
+
+                                    }
+                                    else if (delete_affirmation == "NO")
+                                    {
+                                        Console.WriteLine("Zahtjev odbijen. Vrračamo vas na početni zaslon");
+                                        break;
+                                    }
+                                }
+                            }
+                            if (delete_input!="done")
+                            {
+                                Console.WriteLine("Brisanje je bilo neuspiješno. Pokušajte ponovo.");
+                                Thread.Sleep(2000);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
             case 3:
-                Console.Clear ();
+                while (true)
+                {
+                    Console.Clear();
+                    Console.Write("Upišite ID korisnika kojeg želite urediti: ");
+                    int.TryParse(Console.ReadLine(), out var edit_id);
+                    bool edit_affirmation=false;
+                    foreach(var keys in  Accounts.Keys)
+                    {
+                        if (keys==edit_id)
+                        {
+                            Console.Write($"Trenutno ime je {Accounts[keys].Item1}. Upišite novo koje želite dodati: ");
+                            var new_name = Console.ReadLine();
+                            if (new_name==null)
+                            {
+                                Console.WriteLine("Netočno upisan podatak. Pokušajte ponovno.");
+                                Thread.Sleep(2000);
+                                break;
+                            }
+                            Console.Write($"Trenutno prezime je {Accounts[keys].Item2}. Upišite novo koje želite dodati: ");
+                            var new_surname = Console.ReadLine();
+                            if (new_surname == null)
+                            {
+                                Console.WriteLine("Netočno upisan podatak. Pokušajte ponovno.");
+                                Thread.Sleep(2000);
+                                break;
+                            }
+                            Console.Write($"Trenutni datum rođenja je {Accounts[keys].Item3}. Upišite novi koji želite dodati: ");
+                            DateTime.TryParse(Console.ReadLine(), out var new_birthdate);
+                            if (new_birthdate == null)
+                            {
+                                Console.WriteLine("Netočno upisan podatak. Pokušajte ponovno.");
+                                Thread.Sleep(2000);
+                                break;
+                            }
+                            Accounts[keys] = Tuple.Create( new_name, new_surname, new_birthdate);
+                            Console.WriteLine("Uređivanje je uspiješno, vraćamo vas na početni zaslon");
+                            Thread.Sleep(2000);
+                            edit_affirmation = true;
+                            break;
+
+                        }
+                    }
+                    if (edit_affirmation)
+                    {
+                        break;
+                    }
+                }
                 break;
             case 4:
                 Console.Clear ();
-                for(var i=0; i<Accounts.Count; i++)
+                foreach (var account in Accounts)
                 {
-                    Console.WriteLine(Accounts[i]);
+                    Console.WriteLine(account);
                 }
                 Console.WriteLine("Pritisnite ENTER za povratak na početni zaslon");
                 var home_input=Console.ReadLine();
